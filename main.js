9954475002358942b53395c87c8b6dfb6ab75b2a
JerrodBolton <rodrod1@JerrodBltonsMBP.hsd1.tn.comcast.net>
2021-08-02 15:20:23 -0500

    starting working for Practice

---
+++ main.js
@@ -0,0 +1,23 @@
+// alert("what is today")
+
+function main(){
+
+
+
+}
+var numberOrder1 = [1,2,3,4,5,6,7,8,9,10]
+var numberOrder2 = [1,2,3,4,5,6,7,8,9,10]
+function proaction1(){
+  
+ x = numberOrder1 
+ y = numberOrder2
+
+ for (let index = 0; index < numberOrder1.length; index++) {
+     
+    
+    
+     
+ }
+ 
+  
+}
